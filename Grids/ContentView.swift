3039279de351d9c6a762bc499a650ec68fd5eb19
Benjamin Rojo <benjaminrsalgado@gmail.com>
2025-09-18 11:55:30 -0600

    Initial Commit

---
+++ Grids/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Grids
+//
+//  Created by Benjamin Rojo on 18/09/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Grids/GridsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridsApp.swift
+//  Grids
+//
+//  Created by Benjamin Rojo on 18/09/25.
+//
+
+import SwiftUI
+
+@main
+struct GridsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
