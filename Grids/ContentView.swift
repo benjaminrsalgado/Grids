7363f71e80f82e93e44ae5ccea58f905c84ef1e7
Benjamin Rojo <benjaminrsalgado@gmail.com>
2025-09-18 12:24:21 -0600

    i practice LazyVGrid y LazyHGrid

--- Grids/ContentView.swift
+++ Grids/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         RowGridExample()
+        ColumnGridExample()
     }
 }
 
@@ -29,6 +30,31 @@ struct RowGridExample: View {
                         .font(.system(size: 40))
                         .frame(width: 80, height: 80)
                         .background(Color.green.opacity(0.3))
+                        .cornerRadius(12)
+                }
+            }
+            .padding()
+        }
+    }
+}
+
+
+struct ColumnGridExample: View {
+    let fruits = ["🍎", "🍌", "🍇", "🍓", "🍍", "🥝", "🍒", "🍑"]
+
+    let columns = [
+        GridItem(.flexible()),  // Columna 1
+        GridItem(.flexible())   // Columna 2
+    ]
+
+    var body: some View {
+        ScrollView(.vertical) {  // 👈 Scroll hacia abajo
+            LazyVGrid(columns: columns, spacing: 70) {
+                ForEach(fruits, id: \.self) { fruit in
+                    Text(fruit)
+                        .font(.system(size: 40))
+                        .frame(width: 80, height: 80)
+                        .background(Color.yellow.opacity(0.3))
                         .cornerRadius(12)
                 }
             }
