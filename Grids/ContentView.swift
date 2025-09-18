cbda38cc07f09e769c00c36888641b8b4261333a
Benjamin Rojo <benjaminrsalgado@gmail.com>
2025-09-18 12:18:55 -0600

    i practice lazyHGrid

--- Grids/ContentView.swift
+++ Grids/ContentView.swift
@@ -9,13 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        RowGridExample()
+    }
+}
+
+struct RowGridExample: View {
+    let fruits = ["🍎", "🍌", "🍇", "🍓", "🍍", "🥝", "🍒", "🍑"]
+
+    let rows = [
+        GridItem(.flexible()),  // Fila 1
+        GridItem(.flexible())   // Fila 2
+    ]
+
+    var body: some View {
+        ScrollView(.horizontal) {  // 👈 Scroll hacia la derecha
+            LazyHGrid(rows: rows, spacing: 100) {
+                ForEach(fruits, id: \.self) { fruit in
+                    Text(fruit)
+                        .font(.system(size: 40))
+                        .frame(width: 80, height: 80)
+                        .background(Color.green.opacity(0.3))
+                        .cornerRadius(12)
+                }
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
